558b3c7257e931eac5eee51d9616898705e3526e
alekcole <note.boonanan@gmail.com>
2024-06-07 23:23:39 +0700

    add analytics

--- app.vue
+++ app.vue
@@ -5,8 +5,13 @@
 <script setup>
 const { $fb } = useNuxtApp();
 const facebookPixelCode = "980460796412146,1114858976263485";
-const gtagId = "G-8QD1DD7Y33";
+const gtagId = "GTM-TH6P3MGK";
 const googleSiteCode = "uoxRLwDbag-9GSqoLvnoloP2gBaJhk6LXmj-lpTPNLE";
+import { useAnalytics } from '@nuxtjs/google-analytics'
+
+const gaId = 'G-8QD1DD7Y33' // Replace with your Google Analytics ID
+
+const analytics = useAnalytics()
 
 const { initialize, enableAnalytics } = useGtag();
 
@@ -32,6 +37,10 @@ onMounted(() => {
   if (gtagId) {
     initialize(gtagId);
     enableAnalytics(gtagId);
+  }
+  if(gaId) {
+    analytics.set('id', gaId)
+    analytics.enable()
   }
 });
 </script>
