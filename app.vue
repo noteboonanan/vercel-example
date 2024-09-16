6d12563b01ad58889e844d635ea15c8c368e615a
AlekCole <note.boonanan@gmail.com>
2024-09-16 17:10:55 +0700

    remove gaid

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 const { $fb } = useNuxtApp();
 const facebookPixelCode = "980460796412146,1114858976263485";
 const gtagId = "GTM-TH6P3MGK";
-const gaId = "G-8QD1DD7Y33"; // Replace with your Google Analytics ID
+// const gaId = "G-8QD1DD7Y33"; // Replace with your Google Analytics ID
 const googleSiteCode = "uoxRLwDbag-9GSqoLvnoloP2gBaJhk6LXmj-lpTPNLE";
 // import { useAnalytics } from '@nuxtjs/google-analytics'
 
@@ -22,12 +22,12 @@ useHead({
       content: googleSiteCode,
     },
   ],
-  script: [
-    {
-      src: gaId && `https://www.googletagmanager.com/gtag/js?id=${gaId}`,
-      async: true,
-    },
-  ],
+  // script: [
+  //   {
+  //     src: gaId && `https://www.googletagmanager.com/gtag/js?id=${gaId}`,
+  //     async: true,
+  //   },
+  // ],
 });
 onMounted(() => {
   if (facebookPixelCode && facebookPixelCode.trim() !== "") {
